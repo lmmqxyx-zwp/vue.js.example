49ba05ed8a14848154806347c051e5e66ded7d73
冰羽 <byvaio@outlook.com>
2019-10-17 18:22:39 +0800

    -

---
+++ example/vue-components/blogpost.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="blog-post">
+        <h3>{{ post.title }}</h3>
+        <div v-html="post.content"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'blogpost',
+        props: {
+            post: Object
+        }
+    }
+</script>
+
+<style>
+    .blog-post {
+        background: green;
+    }
+</style>
